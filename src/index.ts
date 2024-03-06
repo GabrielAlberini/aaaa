ebeac0b7d69abd83d9ba7ce10daeeea1d7e21efe
GabrielAlberini <77715611+GabrielAlberini@users.noreply.github.com>
2024-03-06 19:29:06 -0300

    cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
+app.use(cors());
 
 app.get("/", (req: express.Request, res: express.Response) => {
   res.send("Hello World!");
